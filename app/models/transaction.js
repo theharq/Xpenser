46fd724f98e009cfce6d9a13b50c391184abe300
Hildebrando Rueda <harq.soft@gmail.com>
2015-05-15 11:07:09 -0500

    Add fixtures to transaction model

--- app/models/transaction.js
+++ app/models/transaction.js
@@ -1,7 +1,18 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
+var Transaction = DS.Model.extend({
   amount: DS.attr('number'),
-  date: DS.attr('date'),
+  added: DS.attr('date'),
   note: DS.attr('string')
 });
+
+Transaction.reopenClass({
+  FIXTURES: [
+    {id: 1, amount: 1000, added: '2013-02-07T16:44:57', note: 'note 1'},
+    {id: 2, amount: 2000, added: '2013-02-07T16:44:57', note: 'note 2'},
+    {id: 3, amount: 3000, added: '2013-02-07T16:44:57', note: 'note 3'},
+    {id: 4, amount: 4000, added: '2013-02-07T16:44:57', note: 'note 4'}
+  ]
+});
+
+export default Transaction;

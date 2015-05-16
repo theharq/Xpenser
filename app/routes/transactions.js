7e6422c2532e188107e9b7c045139b7819a09041
Hildebrando Rueda <harq.soft@gmail.com>
2015-05-16 15:03:48 -0500

    Create new route index for transactions

--- app/routes/transactions.js
+++ app/routes/transactions.js
@@ -1,7 +1,3 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
-  model: function(){
-    return this.store.findAll('transaction');
-  }
-});
+export default Ember.Route.extend();

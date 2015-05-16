324691c0f123174f7ad2c65ad38b20651410a571
Hildebrando Rueda <harq.soft@gmail.com>
2015-05-16 02:24:31 -0500

    model hook to transactions route
    Display some transactions

--- app/models/transaction.js
+++ app/models/transaction.js
@@ -2,16 +2,16 @@ import DS from 'ember-data';
 
 var Transaction = DS.Model.extend({
   amount: DS.attr('number'),
-  added: DS.attr('date'),
+  date: DS.attr('date'),
   note: DS.attr('string')
 });
 
 Transaction.reopenClass({
   FIXTURES: [
-    {id: 1, amount: 1000, added: '2013-02-07T16:44:57', note: 'note 1'},
-    {id: 2, amount: 2000, added: '2013-02-07T16:44:57', note: 'note 2'},
-    {id: 3, amount: 3000, added: '2013-02-07T16:44:57', note: 'note 3'},
-    {id: 4, amount: 4000, added: '2013-02-07T16:44:57', note: 'note 4'}
+    {id: 1, amount: 1000, date: '2013-02-07', note: 'note 1'},
+    {id: 2, amount: 2000, date: '2013-02-07', note: 'note 2'},
+    {id: 3, amount: 3000, date: '2013-02-07', note: 'note 3'},
+    {id: 4, amount: 4000, date: '2013-02-07', note: 'note 4'}
   ]
 });
 

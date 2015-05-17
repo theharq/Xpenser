00f43d490c95366cf8bf30a2deae998b87bc7f90
Hildebrando Rueda <harq.soft@gmail.com>
2015-05-17 02:10:38 -0500

    calculate amount

--- app/models/transaction.js
+++ app/models/transaction.js
@@ -8,5 +8,13 @@ export default DS.Model.extend({
   category: DS.attr('string'),
   categoryImage: DS.attr('string'),
 
-  amount: Ember.computed.alias('originalAmount')
+  amount: Ember.computed('originalAmount', function(){
+    let amount = this.get('originalAmount');
+    let decimal = /\,\d{2}$/.test(amount) ? ',' : '.';
+    let regex = new RegExp('[^0-9-' + decimal + ']', 'g');
+    return parseFloat(amount
+      .replace(regex, '')    // strip out any cruft
+      .replace(decimal, '.') // make sure decimal point is standard
+    );
+  })
 });

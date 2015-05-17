d0f56db5b1c1d2f28362d4d1fb9c0d801c06e331
Hildebrando Rueda <harq.soft@gmail.com>
2015-05-16 19:10:04 -0500

    Update attributes for Transaction model
    Since we have to parse different information from the body of the sms
    we want to store that information for future references

--- app/models/transaction.js
+++ app/models/transaction.js
@@ -1,7 +1,13 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  amount: DS.attr('number'),
-  date: DS.attr('date'),
-  note: DS.attr('string')
+  originalDate: DS.attr('string'),
+  originalAmount: DS.attr('string'),
+  note: DS.attr('string'),
+  category: DS.attr('string'),
+  categoryImage: DS.attr('string'),
+
+  date: Ember.computed.alias('originalDate'),
+  amount: Ember.computed.alias('originalAmount')
 });

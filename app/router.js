f537141725314ed2489a56ede3464bde1cf96b0a
Hildebrando Rueda <harq.soft@gmail.com>
2015-05-16 19:30:35 -0500

    preserve path, index transitions to transactions

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('transactions', { path: "/" }, function() {});
+  this.route('transactions', function() {});
 });
 
 export default Router;

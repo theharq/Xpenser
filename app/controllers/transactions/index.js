c84d5aaf4766a923a811ecb415b481a266b3ddf2
Hildebrando Rueda <harq.soft@gmail.com>
2015-05-16 20:10:30 -0500

    Remove actual smsImport implementation
    I will refactor this later

--- app/controllers/transactions/index.js
+++ app/controllers/transactions/index.js
@@ -3,29 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     smsImport: function(){
-      var self = this;
-
-      if(SMS) {
-
-        SMS.listSMS({address: '85814', maxCount: 9999}, function(data){
-
-          data.forEach(function(transaction){
-
-            if (/^Bancolombia le informa/.test(transaction.body)){
-              self.store.createRecord('transaction', {
-                note: transaction.body,
-                date: transaction.body.match(/\s(\d{2}\/\d{2}\/\d{4})/)[1],
-                originalAmount: transaction.body.match(/\$([0-9]+\S*)/)[1],
-                categoryImage: "ember.png",
-                category: "Otros"
-              });
-            }
-          });
-
-        }, function(err){
-          alert('error list sms: ' + err);
-        });
-      }
+      alert("to be implemented");
 
       return false;
     }

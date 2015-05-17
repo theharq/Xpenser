73820513503b776ef92a0ef61e3a095e707586c6
Hildebrando Rueda <harq.soft@gmail.com>
2015-05-17 02:10:16 -0500

    filter transactions by current month

--- app/controllers/transactions/index.js
+++ app/controllers/transactions/index.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  filteredTransactions: Ember.computed.filter('model', function(transaction) {
+    return transaction.get('date').getMonth() === new Date().getMonth();
+  }),
+
   actions: {
     smsImport: function(){
       alert("to be implemented");

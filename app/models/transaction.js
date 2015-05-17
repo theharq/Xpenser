0a5b5ce433737d5f0b12439f3626c75c03a55168
Hildebrando Rueda <harq.soft@gmail.com>
2015-05-16 21:53:15 -0500

    replace originalDate with date

--- app/models/transaction.js
+++ app/models/transaction.js
@@ -2,12 +2,11 @@ import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  originalDate: DS.attr('string'),
+  date: DS.attr('date'),
   originalAmount: DS.attr('string'),
   note: DS.attr('string'),
   category: DS.attr('string'),
   categoryImage: DS.attr('string'),
 
-  date: Ember.computed.alias('originalDate'),
   amount: Ember.computed.alias('originalAmount')
 });

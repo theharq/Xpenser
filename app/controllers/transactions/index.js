45687eff759b264dfae5967a18278ca1658d369b
Hildebrando Rueda <harq.soft@gmail.com>
2015-05-18 17:01:29 -0500

    group transactions per day, use new components in template

--- app/controllers/transactions/index.js
+++ app/controllers/transactions/index.js
@@ -6,6 +6,24 @@ export default Ember.Controller.extend({
     return transaction.get('date').getMonth() === new Date().getMonth();
   }),
 
+
+  groupedTransactions: Ember.computed('filteredTransactions', function(){
+    var groupedTransactions = Ember.A();
+    var transactions = this.get('filteredTransactions');
+
+    transactions.forEach(function(transaction){
+      var key = transaction.get('date').toISOString(),
+          group = groupedTransactions.findBy('key', key) || Ember.Object.create({key: key, count: 0, transactions: Ember.A()});
+      if(!groupedTransactions.contains(group)) {
+        groupedTransactions.pushObject(group);
+      }
+      group.incrementProperty('count');
+      group.transactions.pushObject(transaction);
+    });
+
+    return groupedTransactions;
+  }),
+
   actions: {
     smsImport: function(){
       alert("to be implemented");

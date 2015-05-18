30fb5af51b6f82062154dd4c6bad9df4a7886f7e
Hildebrando Rueda <harq.soft@gmail.com>
2015-05-18 18:47:12 -0500

    add format-money helper

---
+++ tests/unit/helpers/format-money-test.js
@@ -0,0 +1,10 @@
+import { formatMoney } from '../../../helpers/format-money';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | format money');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = formatMoney(42);
+  assert.ok(result);
+});

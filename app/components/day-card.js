d53c5ac8435591134778465d20434fa3834d030e
Hildebrando Rueda <harq.soft@gmail.com>
2015-05-18 17:57:42 -0500

    Styling day header

--- app/components/day-card.js
+++ app/components/day-card.js
@@ -5,7 +5,26 @@ export default Ember.Component.extend({
   transactionAmounts: Ember.computed.mapBy('groupedTransaction.transactions', 'amount'),
   amountsTotal: Ember.computed.sum('transactionAmounts'),
 
-  formattedDate: Ember.computed('groupedTransaction.key',function(){
-    return moment(this.get('groupedTransaction.key')).format('dddd D MMMM');
+  date: Ember.computed('groupedTransaction.key',function(){
+    return new Date(this.get('groupedTransaction.key'));
+  }),
+
+  day: Ember.computed('date',function(){
+    return new Date(this.get('date')).getDate();
+  }),
+
+  weekday: Ember.computed('date',function(){
+    let days = ["Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"];
+    return days[new Date(this.get('date')).getDay()];
+  }),
+
+  month: Ember.computed('date',function(){
+    let months = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio",
+                  "Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+    return months[new Date(this.get('date')).getMonth()];
+  }),
+
+  year: Ember.computed('date',function(){
+    return new Date(this.get('date')).getFullYear();
   })
 });

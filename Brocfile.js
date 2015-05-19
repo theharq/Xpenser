b894ae7ff586af8fab0794b1b7bf3f67ce72b6ce
Hildebrando Rueda <harq.soft@gmail.com>
2015-05-19 01:06:55 -0500

    remove ember-ionic addon and add ionic css to Brocfile

--- Brocfile.js
+++ Brocfile.js
@@ -18,5 +18,10 @@ var app = new EmberApp();
 // along with the exports of each module as its value.
 
 app.import('bower_components/moment/moment.js');
+app.import('bower_components/ionic/release/css/ionic.css');
+app.import('bower_components/ionic/release/fonts/ionicons.eot',  {destDir: 'fonts'});
+app.import('bower_components/ionic/release/fonts/ionicons.svg',  {destDir: 'fonts'});
+app.import('bower_components/ionic/release/fonts/ionicons.ttf',  {destDir: 'fonts'});
+app.import('bower_components/ionic/release/fonts/ionicons.woff', {destDir: 'fonts'});
 
 module.exports = app.toTree();
